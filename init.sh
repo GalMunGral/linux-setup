c89e9ea3a37dc84b8870f3d29e68accf86499908
Wenqi He <galmungral@Wenqis-MacBook.local>
2019-12-21 03:30:07 -0500

    Update

--- init.sh
+++ init.sh
@@ -1,2 +1,8 @@
-git clone https://github.com/scrooloose/nerdtree.git ~/.local/share/nvim/site/pack/nerdtree/start/nerdtree
 cp ./init.vim ~/.config/nvim/
+
+# Install NERDTree
+git clone https://github.com/scrooloose/nerdtree.git ~/.local/share/nvim/site/pack/nerdtree/start/nerdtree
+
+# Install solarized color scheme
+cd /usr/local/share/nvim/runtime/colors
+curl -fsSL https://raw.githubusercontent.com/lifepillar/vim-solarized8/master/colors/solarized8_flat.vim -o solarized8_flat.vim
