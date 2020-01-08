d2866289562a69da58d5fdffe96789407af41c4c
Wenqi He <hewenqi96@gmail.com>
2020-01-08 00:47:41 -0500

    Update

--- init.sh
+++ init.sh
@@ -1,21 +1,35 @@
-cp ./init.vim ~/.config/nvim/
+# Install dependencies
+sudo apt update
+sudo apt install git curl wget sed neovim
 
-# Install NERDTree
-dest=~/.local/share/nvim/site/pack/nerdtree/start/nerdtree
-if [[ -d "$dest" ]]; then
-	echo 'NERDTree already installed!'
-else
-	git clone https://github.com/scrooloose/nerdtree.git 
-fi
+# Set up Neovim
+DEST=~/.config/nvim/
+if ! [[ -d $DEST ]]; then mkdir $DEST; fi
+cp ./init.vim $DEST
 
-# Install solarized color scheme
-dl_path=https://raw.githubusercontent.com/lifepillar/vim-solarized8/master/colors/solarized8_flat.vim
-path_1=/usr/share/nvim/runtime/colors/
-path_2=/usr/local/share/nvim/runtime/colors
-if [[ -d "$path_1" ]]; then
-  cd "$path_1"
-else
-  cd "$path_2"
-fi
-echo "Downloading solarized8 into $(pwd)"
-sudo curl -fsSL "$dl_path" -o solarized8_flat.vim
+DEST=~/.local/share/nvim/site/pack/nerdtree/start/nerdtree
+if [[ -d $DEST ]]; then rm -rf $DEST; fi
+git clone https://github.com/scrooloose/nerdtree.git $DEST
+
+sudo curl -fsSL https://raw.githubusercontent.com/lifepillar/vim-solarized8/master/colors/solarized8_flat.vim \
+  -o /usr/share/nvim/runtime/colors/solarized8_flat.vim
+
+# Install browsers
+(
+  cd ../../browser/
+  (cd google-chrome && sudo dpkg -i google-chrome-unstable_current_amd64.deb)
+  (cd tor-browser && tar xvf tor*.tar.xz -C ~ && cd ~/tor* && ./start-tor-browser.desktop --register-app)
+)
+
+# Set up development environment
+(
+  cd ../../dev/
+  (cd node && ./install-13*.sh)
+  (cd sublime && ./install.sh)
+  (cd vscode && sudo dpkg -i code-insiders*.deb)
+)
+
+# Install linux-surface
+git clone --depth 1 https://github.com/jakeday/linux-surface.git ~/linux-surface
+cd ~/linux-surface
+sudo sh setup.sh
